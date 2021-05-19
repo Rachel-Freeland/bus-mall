0ae940e8ecf998958d2dd228278ad26d03d24a4d
Rachel Freeland <rachel.freeland2@gmail.com>
2021-05-18 22:19:58 -0500

    Fixed img naming. Script now renders object to page but, img not loading

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// Global variables & Constants
+//------------------------------------ Global variables & Constants
 
 const voteTallyUlElem = document.getElementById('voteTally');
 const allProductsSectionTag = document.getElementById('allProducts');
@@ -17,6 +17,8 @@ let voteCounter = 0;
 let currentLeftProd = null;
 let currentMidProd = null;
 let currentRightProd = null;
+
+//------------------------------------- Functions
 
 // Create product constructor function
 function Product(name, imgPath) {
@@ -46,7 +48,7 @@ function renderThreeProds(leftProd, midProd, rightProd) {
 }
 
 // pick random products and make sure that all 3 products displayed are different products
-function pickProducts() {
+function pickRandomProducts() {
   const leftProdIndex = Math.floor(Math.random() * Product.allProducts.length);
   const midProdIndex = Math.floor(Math.random() * Product.allProducts.length);
   let rightProdIndex;
@@ -66,11 +68,12 @@ function pickProducts() {
 function renderVoteTally() {
   voteTallyUlElem.innerHTML = '';
   const h2Elem = document.createElement('h2');
-  h2Elem.textContent = 'Voting Results';
+  h2Elem.textContent = 'Voting Results: ';
   voteTallyUlElem.appendChild(h2Elem);
-  for (let product of Product.allProducts) {
+  for (let i =  0; i < Product.allProducts.length; i++) {
+  // for (let Product of Product.allProducts) {
     const liElem = document.createElement('li');
-    liElem.textContent = `${Product.name} : ${Product.votes}`;
+    liElem.textContent = `${Product.name}: ${Product.votes}`;
     voteTallyUlElem.appendChild(liElem);
   }
 }
@@ -86,19 +89,23 @@ function handleClick(e) {
       // add to the image the clicked on
       if (thingTheyClickedOn === leftProdImgTag) {
         currentLeftProd.votes++;
+        currentLeftProd.timesShown++;
       }
       else if (thingTheyClickedOn === midProdImgTag) {
         currentMidProd.votes++;
+        currentMidProd.timeShown++;
       }
       else {
         currentRightProd.votes++;
+        currentRightProd.timesShown++;
       }
       // render new
-      pickProducts();
+      pickRandomProducts();
       renderThreeProds(currentLeftProd, currentMidProd, currentRightProd);
     }
   } else {
     allProductsSectionTag.removeEventListener('click', handleClick);
+    renderVoteTally();
   }
 }
 
@@ -106,28 +113,25 @@ function handleClick(e) {
 allProductsSectionTag.addEventListener('click',handleClick);
 
 
+new Product('R2-D2 Bag', './img/bag.jpg');
+new Product('Banana Slicer', './img/banana.jpg');
+new Product('Bathroom Tablet Stand', './img/bathroom/jpg');
+new Product('Boots', './img/boots.jpg');
+new Product('Breakfast Maker', './img/breakfast.jpg');
+new Product('Meatball Bubblegum', './img/bubblegum.jpg');
+new Product('Chair', './img/chair.jpg');
+new Product('Cthulhu', './img/cthulhu.jpg');
+new Product('Dog-Duck Muzzle', './img/dog-duck.jpg');
+new Product('Dragon Meat', './img/dragon.jpg');
+new Product('Utensil Pen', './img/pen.jpg');
+new Product('Pet Sweep', './img/pet-sweep.jpg');
+new Product('Pizza Scissors', './img/scissors.jpg');
+new Product('Shark Sleeping Bag', './img/shark.jpg');
+new Product('Baby Sweep', './img/sweep.jpg');
+new Product('Tauntaun Sleeping Bag', './img/tauntaun.jpg');
+new Product('Unicorn Meat', './img/unicorn.jpg');
+new Product('Water Can', './img/water-can.jpg');
+new Product('Wine Glass', './img/wine-glass.jpg');
 
-
-
-
-
-new Product('R2-D2 Bag', '../img/bag.jpg.url');
-new Product('Banana Slicer', '../img/banana.jpg.url');
-new Product('Bathroom Tablet Stand', '../img/bathroom/jpg.url');
-new Product('Boots', '../img/boots.jpg.url');
-new Product('Breakfast Maker', '../img/breakfast.jpg.url');
-new Product('Meatball Bubblegum', '../img/bubblegum.jpg.url');
-new Product('Chair', '../img/chair.jpg.url');
-new Product('Cthulhu', '../img/cthulhu.jpg.url');
-new Product('Dog-Duck Muzzle', '../img/dog-duck.jpg.url');
-new Product('Dragon Meat', '../img/dragon.jpg.url');
-new Product('Utensil Pen', '../img/pen.jpg.url');
-new Product('Pet Sweep', '../img/pet-sweep.jpg.url');
-new Product('Pizza Scissors', '../img/scissors.jpg.url');
-new Product('Shark Sleeping Bag', '../img/shark.jpg.url');
-new Product('Baby Sweep', '../img/sweep.jpg.url');
-new Product('Tauntaun Sleeping Bag', '../img/tauntaun.jpg.url');
-new Product('Unicorn Meat', '../img/unicorn.jpg.url');
-new Product('Water Can', '../img/water-can.jpg.url');
-new Product('Wine Glass', '../img/wine-glass.jpg.url');
+pickRandomProducts();
 renderThreeProds(currentLeftProd, currentMidProd, currentRightProd);
